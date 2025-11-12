516167b8de6ca15c9ef1e928dabab9f58501b677
StreamEcho <119097478+LS-Station@users.noreply.github.com>
2025-11-12 12:53:37 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'streamecho-v1';
+const urlsToCache = [
+  '/',
+  '/streamecho-pwa/icon-192.png',
+  '/streamecho-pwa/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
